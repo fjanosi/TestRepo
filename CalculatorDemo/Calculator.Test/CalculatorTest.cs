8fb49d37e80c1456e17506ec7e861e005e734358
ferenc.janosi@logmein.com <ferenc.janosi@logmein.com>
2014-10-23 23:23:03 +0200

    create calculator app

---
+++ CalculatorDemo/Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        public TestContext TestContext { get; set; }
+        
+        [TestMethod]
+        public void Add_Success()
+        {
+            int a = 5;
+            int b = 10;
+            int result = Calculator.Add(a, b);
+
+            Assert.AreEqual(a + b, result);
+
+            TestContext.WriteLine("Test run successfully");
+
+        }
+    }
+}
---
+++ CalculatorDemo/CalculatorApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int result = Calculator.Calculator.Add(a, b);
+
+            //String message = String.Format("{0} + {1} = {2}", a, b, result);
+            Console.WriteLine("{0} + {1} = {2}", a, b, result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
